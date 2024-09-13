3bdae2bcb5a5496d7e805a7daf2e4351582760d0
Tobey Blaber <contact@tobey.me>
2024-09-13 22:41:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# Tobey's Game Info Fix for BepInEx.MelonLoader.Loader
+
+A simple patch to ensure the game info logs of [BepInEx.MelonLoader.Loader](https://github.com/BepInEx/BepInEx.MelonLoader.Loader) are correct.
+
+## Why?
+
+It's nice to have the correct game info in the logs, it's useful for troubleshooting.
+
+Unfortunately, the version of MelonLoader shipped with BepInEx.MelonLoader.Loader uses [AssetsTools.NET](https://github.com/nesrak1/AssetsTools.NET) to parse the info from the `globalgamemanagers` file manually, which is only going to work correctly assuming the version of AssetsTools.NET installed has up-to-date information on how to parse that file, which it often does not. This leads to incorrect information sometimes being parsed from the file.
+
+To address this, Tobey.MLLoader.GameInfo relies on Unity's own API for gathering the same information, which is available since at least Unity 5.6. Of course, this relies on Unity to have been initialised far enough to have parsed this information itself. This only works as long as MelonLoader is gathering the info after Unity has been initialised enough to have parsed it, which it usually is by the time BepInEx.MelonLoader.Loader is loaded.
+
+## Usage
+
+Just plop the contents of the downloaded .zip from [the releases page](https://github.com/toebeann/Tobey.MLLoader.GameInfo/releases) into your game folder (after installing [BepInEx](https://github.com/BepInEx/BepInEx) and [BepInEx.MelonLoader.Loader](https://github.com/BepInEx/BepInEx.MelonLoader.Loader), of course).
---
+++ Tobey.MLLoader.GameInfo/Patcher.cs
@@ -0,0 +1,78 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Tobey.MLLoader.GameInfo;
+
+[HarmonyWrapSafe]
+public static class Patcher
+{
+    // Without the contents of this region, the patcher will not be loaded by BepInEx - do not remove!
+    #region BepInEx Patcher Contract
+    public static IEnumerable<string> TargetDLLs { get; } = [];
+    public static void Patch(AssemblyDefinition _) { }
+    #endregion
+
+    private static Harmony? harmony;
+
+    private static ManualLogSource? logger = Logger.CreateLogSource("Tobey.MLLoader.GameInfo");
+
+    // entry point - do not delete or rename!
+    public static void Finish()
+    {
+        try
+        {
+            Assembly.Load("MelonLoader");
+            harmony = Harmony.CreateAndPatchAll(typeof(Patcher));
+        }
+        catch (FileNotFoundException)
+        {
+            logger!.LogWarning("MelonLoader was not found, patch skipped.");
+            Logger.Sources.Remove(logger);
+            logger!.Dispose();
+            logger = null;
+        }
+    }
+
+    public static MethodBase TargetMethod() => AccessTools.Method("MelonLoader.InternalUtils.UnityInformationHandler:ReadGameInfo");
+
+    public static void Postfix()
+    {
+        try
+        {
+            var unityInformationHandler = Traverse.CreateWithType("MelonLoader.InternalUtils.UnityInformationHandler");
+            var application = Traverse.CreateWithType("UnityEngine.Application");
+
+            var getValue = (string name) => application.Property(name) switch
+            {
+                Traverse t when t.PropertyExists() => t.GetValue<string>(),
+                _ => application.Field<string>(name).Value,
+            };
+
+            unityInformationHandler.Property<string>("GameVersion").Value = getValue("version");
+            unityInformationHandler.Property<string>("GameName").Value = getValue("productName");
+            unityInformationHandler.Property<string>("GameDeveloper").Value = getValue("companyName");
+            unityInformationHandler.Property("EngineVersion").SetValue(
+                Traverse.CreateWithType("AssetRipper.VersionUtilities.UnityVersion")
+                .Method("Parse", [getValue("unityVersion")]).GetValue());
+
+            logger!.LogMessage("Successfully patched MelonLoader game info.");
+        }
+        catch (Exception ex)
+        {
+            logger!.LogError(ex);
+        }
+        finally
+        {
+            harmony!.UnpatchSelf();
+            harmony = null;
+            Logger.Sources.Remove(logger);
+            logger!.Dispose();
+            logger = null;
+        }
+    }
+}
